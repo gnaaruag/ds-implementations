c1bba291b8d0f237cd36066e5b862d98651d2d87
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-06-17 11:46:37 +0530

    add doubly linked lists

---
+++ doublelinkedlist/Node.java
@@ -0,0 +1,30 @@
+package doublelinkedlist;
+
+/**
+ * A node in a singly linked list.
+ *
+ * @param <T> the type of element held in this node
+ */
+class Node<T> {
+    /**
+     * The data stored in this node.
+     */
+    T data;
+
+    /**
+     * The next node in the linked list.
+     */
+    Node<T> next;
+    Node<T> prev;
+
+    /**
+     * Constructs a new node with the specified data.
+     *
+     * @param data the data to be stored in this node
+     */
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
